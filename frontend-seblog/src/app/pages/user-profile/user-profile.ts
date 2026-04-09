d0cfcd14ec5ecc36f75c6d13e9afcb9e73641f97
Serge <sshibkojavadev@gmail.com>
2026-04-09 18:44:12 +0800

    FE:FEATURE: add user-profile component

--- frontend-seblog/src/app/pages/user-profile/user-profile.ts
+++ frontend-seblog/src/app/pages/user-profile/user-profile.ts
@@ -5,31 +5,42 @@ import { AuthService } from '../../services/auth.service';
 import { PostService } from '../../services/post.service';
 import { Post } from '../../models/post.model';
 import { User } from '../../models/user.model';
+import { RouterLink } from '@angular/router';
+import { UserProfileService } from '../../services/user-profile.service';
+import { UserProfileUpdate } from '../../models/user-profile.model';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './user-profile.html',
   styleUrls: ['./user-profile.scss']
 })
 export class ProfileComponent implements OnInit {
   private authService = inject(AuthService);
   private postService = inject(PostService);
+  private userProfileService = inject(UserProfileService);
   private fb = inject(FormBuilder);
   
-  user = signal<User | null>(null);
-  userPosts = signal<Post[]>([]);
-  isLoading = signal(true);
-  isEditing = signal(false);
+  //user = signal<User | null>(null);
+  //userPosts = signal<Post[]>([]);
+  isLoading = signal(false);
+  //isEditing = signal(false);
+  errorMessage = signal<string>('');
+  successMessage = signal<string>('')
   
   profileForm: FormGroup;
   
   constructor() {
     this.profileForm = this.fb.group({
-      username: ['', [Validators.required, Validators.minLength(3)]],
-      email: ['', [Validators.required, Validators.email]],
-      bio: ['', Validators.maxLength(500)]
+      displayName: ['', [Validators.required, Validators.minLength(3)]],
+      firstName: [''],
+      lastName: [''],
+      birthDate: [''],
+      phone: [''],
+      bio: [''],
+      location: [''],
+      optionalEmail: ['', [Validators.email]],
     });
   }
   
@@ -37,70 +48,151 @@ export class ProfileComponent implements OnInit {
     this.loadProfile();
   }
   
-  loadProfile(): void {
-    this.user.set(this.authService.user());
-    
-    if (this.user()) {
-      this.profileForm.patchValue({
-        username: this.user()?.username,
-        email: this.user()?.email,
-        bio: this.user()?.bio || ''
-      });
-      
-      this.loadUserPosts();
-    }
-  }
-  
-  loadUserPosts(): void {
-    this.isLoading.set(true);
-    
-    // Здесь будет загрузка постов пользователя
-    setTimeout(() => {
-      this.userPosts.set([
-        {
-          id: "1",
-          title: 'Пример поста',
-          content: 'Содержимое поста...',
-          //excerpt: 'Краткое описание...',
-          slug: 'primer-posta',
-          author: this.user()!,
-          postType: 'Программирование',
-          tags: ['angular', 'typescript'],
-          commentsCount: 5,
-          likesCount: 10,
-          viewsCount: 100,
-          isPublished: true,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          status: ''
+  private loadProfile(): void {
+    this.userProfileService.getProfile().subscribe({
+      next: (response) => {
+        if (response.success && response.data) {
+          this.profileForm.patchValue({
+            displayName: response.data.displayName || '',
+            firstName: response.data.firstName || '',
+            lastName: response.data.lastName || '',
+            birthDate: response.data.birthDate || '',
+            phone: response.data.phone || '',
+            bio: response.data.bio || '',
+            location: response.data.locationAt || '',
+            optionalEmail: response.data.optionalEmail || ''
+          });
         }
-      ]);
-      this.isLoading.set(false);
-    }, 1000);
+      },
+      error: (err) => {
+        this.errorMessage.set(err.error?.message || 'Ошибка загрузки профиля');
+      }
+    });
   }
-  
-  enableEdit(): void {
-    this.isEditing.set(true);
+
+  onSubmit(): void {
+    if (this.profileForm.invalid) {
+      this.markFormAsTouched();
+      return;
+    }
+    this.isLoading.set(true);
+    this.errorMessage.set('');
+    this.successMessage.set('');
+
+    const formData: UserProfileUpdate = this.profileForm.value;
+
+    this.userProfileService.updateProfile(formData).subscribe({
+      next: (response) => {
+        this.isLoading.set(false);
+        if (response.success) {
+          this.successMessage.set('Профиль успешно обновлен');
+          setTimeout(() => this.successMessage.set(''), 300);
+        }
+      },
+      error: (err) => {
+        this.isLoading.set(false);
+        this.errorMessage.set(err.error?.message || 'Ошибка обновления профиля');
+      }
+    });
   }
-  
-  saveProfile(): void {
-    if (this.profileForm.invalid) return;
-    
-    // Здесь будет обновление профиля через API
-    console.log('Profile updated:', this.profileForm.value);
-    
-    this.isEditing.set(false);
-  }
-  
-  cancelEdit(): void {
-    this.isEditing.set(false);
-    this.loadProfile(); // Сбрасываем форму
-  }
-  
-  deleteAccount(): void {
-    if (confirm('Вы уверены? Это действие нельзя отменить.')) {
-      // Здесь будет удаление аккаунта
-      this.authService.logout();
+
+  onAvatarUpload(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    const file = input.files?.[0];
+
+    if (file) {
+      if(!file.type.startsWith('image/')) {
+        this.errorMessage.set('Пожалуйста, выберите изображение');
+        return;
+      }
+
+      if (file.size > 5 * 1024 * 1024) {
+        this.errorMessage.set('Файл слишком боольшой. Максимум 5 МБ');
+        return;
+      }
+
+      this.userProfileService.uploadAvatar(file).subscribe({
+        next: (response) => {
+          if (response.success) {
+            this.successMessage.set('Аватар успешно загружен');
+            setTimeout(() => this.successMessage.set(''), 3000);
+          }
+        },
+        error: (err) => {
+          this.errorMessage.set(err.error?.message || 'Ошибка загрузки аватара');
+        }
+      });
     }
   }
+
+  private markFormAsTouched(): void {
+    Object.values(this.profileForm.controls).forEach(control => {
+      control.markAsTouched();
+    })
+  }
+
+  get user() {
+    return this.authService.user();
+  }
+
+  get profile() {
+    return this.userProfileService.profile();
+  }
+
+
+  
+
+
+  // loadUserPosts(): void {
+  //   this.isLoading.set(true);
+    
+  //   // Здесь будет загрузка постов пользователя
+  //   setTimeout(() => {
+  //     this.userPosts.set([
+  //       {
+  //         id: "1",
+  //         title: 'Пример поста',
+  //         content: 'Содержимое поста...',
+  //         //excerpt: 'Краткое описание...',
+  //         slug: 'primer-posta',
+  //         author: this.user()!,
+  //         postType: 'Программирование',
+  //         tags: ['angular', 'typescript'],
+  //         commentsCount: 5,
+  //         likesCount: 10,
+  //         viewsCount: 100,
+  //         isPublished: true,
+  //         createdAt: new Date(),
+  //         updatedAt: new Date(),
+  //         status: ''
+  //       }
+  //     ]);
+  //     this.isLoading.set(false);
+  //   }, 1000);
+  // }
+  
+  // enableEdit(): void {
+  //   this.isEditing.set(true);
+  // }
+  
+  // saveProfile(): void {
+  //   if (this.profileForm.invalid) return;
+    
+  //   // Здесь будет обновление профиля через API
+  //   console.log('Profile updated:', this.profileForm.value);
+    
+  //   this.isEditing.set(false);
+  // }
+  
+  // cancelEdit(): void {
+  //   this.isEditing.set(false);
+  //   this.loadProfile(); // Сбрасываем форму
+  // }
+  
+  // deleteAccount(): void {
+  //   if (confirm('Вы уверены? Это действие нельзя отменить.')) {
+  //     // Здесь будет удаление аккаунта
+  //     this.authService.logout();
+  //   }
+  // }
 }
